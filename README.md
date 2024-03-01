4897ba17741c732552f39081f1006d2043454835
Jesse <jessecdenier@gmail.com>
2024-03-01 13:11:50 -0600

    Built out first functioning model using 4 functions and checks on the user input.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Installation
 
-First this repository must be cloned to your local computer. Then running vscode or a similar software you must run the command lines 'npm init -y', 'npm i inquirer@8.2.4', 'npm i jest@29.7.0', 'npm i inquirer-maxlength-input-prompt'. Once all of that has been completed you can run command line 'node index.js' and answer all the questions provided to you to create a logo.
+First this repository must be cloned to your local computer. Then running vscode or a similar software you must run the command lines 'npm init -y', 'npm i inquirer@8.2.4', 'npm i jest@29.7.0', 'npm i inquirer-maxlength-input-prompt@1.0.2'. Once all of that has been completed you can run command line 'node index.js' and answer all the questions provided to you to create a logo.
--- index.js
+++ index.js
@@ -6,25 +6,256 @@ const fs = require("fs");
 const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
 inquirer.registerPrompt("maxLength", MaxLengthInputPrompt);
 
-// TODO: Describe and Complete Function.
-function createText(answers) {}
+// Constant values for user inputs to be compared to and checked for validity.
+const hexRegex = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+const keywordColors = [
+  `alicealue`,
+  `antiquewhite`,
+  `aqua`,
+  `aquamarine`,
+  `azure`,
+  `beige`,
+  `bisque`,
+  `black`,
+  `blanchedalmond`,
+  `blue`,
+  `blueviolet`,
+  `brown`,
+  `burlywood`,
+  `cadetblue`,
+  `chartreuse`,
+  `chocolate`,
+  `coral`,
+  `cornflowerblue`,
+  `cornsilk`,
+  `crimson`,
+  `cyan`,
+  `darkblue`,
+  `darkcyan`,
+  `darkgoldenrod`,
+  `darkgray`,
+  `darkgrey`,
+  `darkgreen`,
+  `darkkhaki`,
+  `darkmagenta`,
+  `darkolivegreen`,
+  `darkorange`,
+  `darkorchid`,
+  `darkred`,
+  `darksalmon`,
+  `darkseagreen`,
+  `darkslateblue`,
+  `darkslategray`,
+  `darkslategrey`,
+  `darkturquoise`,
+  `darkviolet`,
+  `deeppink`,
+  `deepskyblue`,
+  `dimgray`,
+  `dimgrey`,
+  `dodgerblue`,
+  `firebrick`,
+  `floralwhite`,
+  `forestgreen`,
+  `fuchsia`,
+  `gainsboro`,
+  `ghostwhite`,
+  `gold`,
+  `goldenrod`,
+  `gray`,
+  `grey`,
+  `green`,
+  `greenyellow`,
+  `honeydew`,
+  `hotpink`,
+  `indianred`,
+  `indigo`,
+  `ivory`,
+  `khaki`,
+  `lavender`,
+  `lavenderblush`,
+  `lawngreen`,
+  `lemonchiffon`,
+  `lightblue`,
+  `lightcoral`,
+  `lightcyan`,
+  `lightgoldenrodyellow`,
+  `lightgray`,
+  `lightgrey`,
+  `lightgreen`,
+  `lightpink`,
+  `lightsalmon`,
+  `lightseagreen`,
+  `lightskyblue`,
+  `lightslategray`,
+  `lightslategrey`,
+  `lightsteelblue`,
+  `lightyellow`,
+  `lime`,
+  `limegreen`,
+  `linen`,
+  `magenta`,
+  `maroon`,
+  `mediumaquamarine`,
+  `mediumblue`,
+  `mediumorchid`,
+  `mediumpurple`,
+  `mediumseagreen`,
+  `mediumslateblue`,
+  `mediumspringgreen`,
+  `mediumturquoise`,
+  `mediumvioletred`,
+  `midnightblue`,
+  `mintcream`,
+  `mistyrose`,
+  `moccasin`,
+  `navajowhite`,
+  `navy`,
+  `oldlace`,
+  `olive`,
+  `olivedrab`,
+  `orange`,
+  `orangered`,
+  `orchid`,
+  `palegoldenrod`,
+  `palegreen`,
+  `paleturquoise`,
+  `palevioletred`,
+  `papayawhip`,
+  `peachpuff`,
+  `peru`,
+  `pink`,
+  `plum`,
+  `powderblue`,
+  `purple`,
+  `red`,
+  `rosybrown`,
+  `royalblue`,
+  `saddlebrown`,
+  `salmon`,
+  `sandybrown`,
+  `seagreen`,
+  `seashell`,
+  `sienna`,
+  `silver`,
+  `skyblue`,
+  `slateblue`,
+  `slategray`,
+  `slategrey`,
+  `snow`,
+  `springgreen`,
+  `steelblue`,
+  `tan`,
+  `teal`,
+  `thistle`,
+  `tomato`,
+  `turquoise`,
+  `violet`,
+  `wheat`,
+  `white`,
+  `whitesmoke`,
+  `yellow`,
+  `yellowgreen`,
+];
 
-// TODO: Describe and Complete Function.
-function setTextColor(answers) {}
+// Initially creates svgMarkup based on users choice of shape.
+function createShape(answers) {
+  let svgMarkup = "";
+  if (answers.shape === "Circle") {
+    svgMarkup = `
+        <svg width="300" height="200">
+          <circle cx="150" cy="100" r="75" fill="shapecolor" />
+          <text x="50%" y="58%" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="40" fill="textcolor">userText</text>
+        </svg>`;
+  }
+  if (answers.shape === "Triangle") {
+    svgMarkup = `
+        <svg width="300" height="200">
+          <polygon points="150,37.5 225,162.5 75,162.5" fill="shapecolor"/>
+          <text x="50%" y="64%" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="40" fill="textcolor">userText</text>
+        </svg>`;
+  }
+  if (answers.shape === "Square") {
+    svgMarkup = `
+        <svg width="300" height="200">
+          <rect x="75" y="25" width="150" height="150" fill="shapecolor" />
+          <text x="50%" y="58%" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="40" fill="textcolor">userText</text>
+        </svg>`;
+  }
+  return svgMarkup;
+}
 
-// TODO: Describe and Complete Function.
-function createShape(answers) {}
+// Changes shape color in SVG Markup based on users choices.
+function setShapeColor(svgMarkup, answers) {
+  let color = answers.shapeColor.toLowerCase();
+  svgMarkup = svgMarkup.replace(`fill="shapecolor"`, `fill="${color}"`);
+  return svgMarkup;
+}
 
-// TODO: Describe and Complete Function.
-function setShapeColor(answers) {}
+// Changes placeholder text in SVG Markup based on users choices.
+function createText(svgMarkup, answers) {
+  let acronym = answers.text.toUpperCase();
+  svgMarkup = svgMarkup.replace(`userText`, `${acronym}`);
+  return svgMarkup;
+}
 
-// TODO: Describe and Complete Function.
-function createLogo(answers) {}
+// Changes text color in SVG Markup based on users choices.
+function setTextColor(svgMarkup, answers) {
+  let color = answers.textColor.toLowerCase();
+  svgMarkup = svgMarkup.replace(`fill="textcolor"`, `fill="${color}"`);
+  return svgMarkup;
+}
+
+// Saves the final svgMarkup as a file named Logo.svg
+function createLogo(svgMarkup) {
+  fs.writeFile("Logo.svg", svgMarkup, (err) => {
+    if (err) throw err;
+    console.log("Logo Created.");
+  });
+}
 
 // Asks the user for choices regarding logo creation and then runs all logo creation functions.
 function init() {
   inquirer
     .prompt([
+      {
+        name: "shape",
+        message: "What shape would you like the logo to be?",
+        type: "list",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        name: "colorSelection",
+        message:
+          "Do you want to pick your colors with keywords or hexadecimal values?",
+        type: "list",
+        choices: ["Keywords", "Hexadecimal Values"],
+      },
+      {
+        name: "shapeColor",
+        message: "What color would you like the shape to be?",
+        when: (answers) => answers.colorSelection === "Keywords",
+        validate: function (input) {
+          if (keywordColors.includes(input.toLowerCase())) {
+            return true;
+          } else {
+            return "Sorry that keyword doesn't work. Please enter a valid color like red, blue, seagreen, etc.";
+          }
+        },
+      },
+      {
+        name: "shapeColor",
+        message:
+          "What color would you like the shape to be? Enter a hexadecimal value.",
+        when: (answers) => answers.colorSelection === "Hexadecimal Values",
+        validate: function (input) {
+          if (hexRegex.test(input)) {
+            return true;
+          } else {
+            return "Please enter a valid hexadecimal color value formatted as #XXX or #XXXXXXX";
+          }
+        },
+      },
       {
         name: "text",
         message:
@@ -34,28 +265,40 @@ function init() {
       },
       {
         name: "textColor",
-        message:
-          "What color would you like the text to be? Enter hexadecimal number or keyword.",
+        message: "What color would you like the text to be?",
+        when: (answers) => answers.colorSelection === "Keywords",
+        validate: function (input) {
+          if (keywordColors.includes(input.toLowerCase())) {
+            return true;
+          } else {
+            return "Sorry that keyword doesn't work. Please enter a valid color like red, blue, seagreen, etc.";
+          }
+        },
       },
       {
-        name: "shape",
-        message: "What shape would you like the logo to be?",
-        type: "list",
-        choices: ["Circle", "Triangle", "Square"],
-      },
-      {
-        name: "shapeColor",
+        name: "textColor",
         message:
-          "What color would you like the shape to be? Enter hexadecimal number or keyword.",
+          "What color would you like the text to be? Enter a hexadecimal value.",
+        when: (answers) => answers.colorSelection === "Hexadecimal Values",
+        validate: function (input) {
+          if (hexRegex.test(input)) {
+            return true;
+          } else {
+            return "Please enter a valid hexadecimal color value formatted as #XXX or #XXXXXXX";
+          }
+        },
       },
     ])
     .then((answers) => {
-      createText(answers);
-      setTextColor(answers);
       createShape(answers);
-      setShapeColor(answers);
-      createLogo(answers);
-      console.log("Generated logo.svg");
+      let svgMarkup = createShape(answers);
+      setShapeColor(svgMarkup, answers);
+      svgMarkup = setShapeColor(svgMarkup, answers);
+      createText(svgMarkup, answers);
+      svgMarkup = createText(svgMarkup, answers);
+      setTextColor(svgMarkup, answers);
+      svgMarkup = setTextColor(svgMarkup, answers);
+      createLogo(svgMarkup);
     });
 }
 
